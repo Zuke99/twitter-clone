e27d4d1c0c093476f5d593abcb25c9c0619eb820
Dheeraj P <dheerajp@Dheerajs-MacBook-Pro.local>
2023-09-17 13:16:32 +0530

    Added Google OAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import type { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+    <GoogleOAuthProvider clientId='2026621704-3lpdsc112d21521qng3j4ndt6k7oaraq.apps.googleusercontent.com'>
+    <Component {...pageProps} />
+    <Toaster />
+    </GoogleOAuthProvider>
+    </>
+)}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,12 @@
 import { Inter } from 'next/font/google'
 import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from 'react-icons/bs'
 import { BiHash, BiHomeCircle, BiUser } from 'react-icons/bi'
-import React from 'react'
+import React, { useCallback } from 'react'
 import FeedCard from '@/components/FeedCard'
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google'
+import toast from 'react-hot-toast'
+import { graphqlClient } from '@/clients/api'
+import { verifyUserGoogleTokenQuery } from '@/graphql/queries/user'
 
 
 interface TwitterSidebarButton {
@@ -38,10 +42,23 @@ const sideBarMenuItems: TwitterSidebarButton[] = [
   }
 ]
 
+
+
 export default function Home() {
+  const handleLoginWithGoogle = useCallback( async (cred: CredentialResponse) =>{
+    const googleToken = cred.credential;
+    if(!googleToken) return toast.error(`Google token not found`);
+
+    const { verifyGoogleToken } = await graphqlClient.request( verifyUserGoogleTokenQuery, { token: googleToken})
+    toast.success("Verification Success");
+    console.log(verifyGoogleToken);
+
+    if(verifyGoogleToken) window.localStorage.setItem('__twitter_token',verifyGoogleToken)
+
+  },[])
   return (
     <div className='grid grid-cols-12 h-screen w-screen px-56'>
-      <div className='  col-span-3 pt-8'>
+      <div className='col-span-3 pt-8'>
         <div className="text-4xl h-fit hover:bg-gray-900 p-2 cursor-pointer rounded-full transition-all">
           <BsTwitter />
         </div>
@@ -79,7 +96,12 @@ export default function Home() {
         <FeedCard/>
         <FeedCard/>
       </div>
-      <div className='col-span-4'></div>
+      <div className='col-span-4 p-5'>
+        <div className=' p-5 bg-slate-700 rounded-lg'>
+          <h1 className=''> New to Twitter?</h1>
+          <GoogleLogin onSuccess={handleLoginWithGoogle}></GoogleLogin>
+        </div>
+      </div>
     </div>
 
   )
